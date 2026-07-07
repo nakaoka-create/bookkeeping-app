fdeb832fedf2ba3e833040692d2ec9fb6e22ca0b
nakaoka-create <999ramenman@gmail.com>
2026-07-07 13:05:29 +0900

    a

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBop68LEI2nXKMglq1VJ-_7YiGfo0SLRuk",
-  authDomain: "kakeibo-hukusiki.firebaseapp.com",
-  projectId: "kakeibo-hukusiki",
-  storageBucket: "kakeibo-hukusiki.firebasestorage.app",
-  messagingSenderId: "803801033427",
-  appId: "1:803801033427:web:3821f37bc2cf87e5944226"
-};
